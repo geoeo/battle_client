6e3caa50fa478217447d1cb9848eebfb6467f58d
Marc <marc.dhaubenstock@gmail.com>
2014-11-16 17:05:53 +0100

    first commit

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationController : MonoBehaviour {
+
+	public SkeletonAnimation skeletonAnimation;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		skeletonAnimation.AnimationName = "walk";
+	
+	}
+}
